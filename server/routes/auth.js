cc81861cc46d642b8031065f18764d9fbe378365
Ross <oblivion1214@hotmail.com>
2018-01-13 18:28:15 -0500

    Added proper require statements for authentication route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+// const InstagramStrategy = require('passport-instagram').Strategy;
+// const LocalStrategy = require('passport-local').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
+// const TwitterStrategy = require('passport-twitter').Strategy;
+// const GitHubStrategy = require('passport-github').Strategy;
+// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+// const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+// const OpenIDStrategy = require('passport-openid').Strategy;
+// const OAuthStrategy = require('passport-oauth').OAuthStrategy;
+// const OAuth2Strategy = require('passport-oauth').OAuth2Strategy;
 
 /**
  * OAuth authentication routes. (Sign in)
